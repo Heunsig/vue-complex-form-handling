c62bea83d49a9f97f4bbf10e7184d35ae1302245
Heunsig <heun3344@gmail.com>
2024-05-28 20:34:18 +0900

    init vue3

---
+++ src/components/PersonalInfoForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Personal Info Form</h1>
+    <div>
+      <label for="first_name">First Name:</label>
+      <input type="text" id="first_name" />
+    </div>
+    <div>
+      <label for="last_name">Last Name:</label>
+      <input type="text" id="last_name" />
+    </div>
+    <div>
+      <!-- Date of birth -->
+      <label for="dob">Date of Birth:</label>
+      <input type="date" id="dob" />
+    </div>
+    <div>
+      <!-- Gender selector -->
+      <label for="gender">Gender</label>
+      <select>
+        <option>Select Gender</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+    </div>
+  </div>
+</template>
---
+++ src/views/BasicStyle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PersonalInfoForm from '@/components/PersonalInfoForm.vue'
+</script>
+<template>
+  <div>
+    <h1>Basic Style</h1>
+    <div>
+      <PersonalInfoForm />
+    </div>
+  </div>
+</template>
